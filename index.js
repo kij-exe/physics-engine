0def16116cbb178be0c9869f3baa0948929e5c19
kij-exe <themrnikyta@gmail.com>
2023-08-17 18:50:47 +0100

    two worlds separated vector functions added

--- index.js
+++ index.js
@@ -6,17 +6,13 @@ import Ball from "./source/bodies/Ball.js"
 import Projection from "./source/worlds/Projection.js"
 import Raycast from "./source/worlds/Raycast.js"
 
-// var canvas = document.getElementById("canvas1");
-// var ctx = canvas.getContext("2d");
-// ctx.beginPath();
-// ctx.arc(95, 50, 40, 0, 2 * Math.PI);
-// ctx.fill();
-
 var DOM_ids = {
     canvas_id: "canvas1"
 }
 
 var sim = new Simulation(DOM_ids);
+
+sim.grid.ctx.lineWidth = 3;
 
 // sim.redraw();
 
@@ -42,120 +38,6 @@ function finishRoutine(accumulatedTime) {
     
     console.log("Accumulated time: " + accumulatedTime);
 }
-
-// var pq = new PriorityQueue();
-
-sim.grid.ctx.lineWidth = 3;
-
-var v = new Vector(20, 10);
-var c = new Vector(350, 250);
-
-// v.redraw(sim.grid, c)
-
-// var a = new Vector(200, 300);
-// var b = new Vector(400, 400);
-
-var a;
-var b;
-
-var c;
-
-var wall;
-
-var k;
-
-// var k = (b.cross(c) + c.cross(a) + a.cross(b)) /
-// (v.cross(b) + a.cross(v))
-
-console.log(k)
-
-// v.multiply(k);
-// v.redraw(sim.grid, c, "Red")
-
-var control = 0;
-
-function raycast(e) {
-    var x = e.x - sim.canvas.getBoundingClientRect().x - 2.727;
-    var y = sim.canvas.height - (e.y - sim.canvas.getBoundingClientRect().y - 2.727);
-
-    sim.ctx.clearRect(0, 0, sim.canvas.width, sim.canvas.height)
-
-    if (control == 0) {
-        a = new Vector(x, y);
-        control++;
-        return;
-    }
-
-    if (control == 1) {
-        b = new Vector(x, y);
-        wall = new Wall(a, b);
-        wall.redraw(sim.grid);
-        control++;
-        return;
-    }
-
-    wall.redraw(sim.grid);
-
-    if (control == 2) {
-        c = new Vector(x, y);
-        control++;
-        return;
-    }
-
-
-    var v = new Vector(x, y).subtracted(c)
-    // v.redraw(sim.grid, c)
-
-    var k = (b.cross(c) + c.cross(a) + a.cross(b)) /
-    (v.cross(b) + a.cross(v))
-
-    // v.multiplied(k).redraw(sim.grid, c, `rgba(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, 100)`)
-
-    v.multiplied(k).redraw(sim.grid, c)
-
-    new Ball(30, v.multiplied(k).added(c)).redraw(sim.grid)
-}
-
-function projection(e) {
-    var x = e.x - sim.canvas.getBoundingClientRect().x - 2.727;
-    var y = sim.canvas.height - (e.y - sim.canvas.getBoundingClientRect().y - 2.727);
-
-    sim.ctx.clearRect(0, 0, sim.canvas.width, sim.canvas.height)
-
-    if (control == 0) {
-        a = new Vector(x, y);
-        c = a;
-        control++;
-        return;
-    }
-
-    if (control == 1) {
-        b = new Vector(x, y);
-        wall = new Wall(a, b);
-        wall.redraw(sim.grid);
-        control++;
-        return;
-    }
-
-    wall.redraw(sim.grid);
-
-    var v = new Vector(x, y).subtracted(c)
-    v.redraw(sim.grid, c)
-
-    var ab = b.subtracted(a);
-
-    var k = ab.dot(v) / ab.lengthSquared();
-
-    var proj = ab.multiplied(k)
-
-    proj.redraw(sim.grid, c, "Red")
-
-    var perp = v.subtracted(proj);
-    
-    perp.redraw(sim.grid, c.added(proj), "Red")
-}
-
-// sim.canvas.addEventListener("mousedown", projection)
 
 document.getElementById("button_clear").addEventListener(
     "click",
@@ -189,3 +71,4 @@ document.getElementById("raycast").addEventListener(
         startWorld(new Raycast(sim.canvas, sim.grid));
     }
 )
+
